8ffa49d9b4bd8e1aa5dc80b5edc33fbf22c9379c
unknown <alieldeep111@gmail.com>
2024-04-07 01:41:03 +0200

    updating SQL queries

--- SQL QUERIES/user.sql
+++ SQL QUERIES/user.sql
@@ -1,11 +1,12 @@
-create database market;
+CREATE DATABASE IF NOT EXISTS market;
 use market;
+drop table IF EXISTS user;
 CREATE TABLE user (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    userName VARCHAR(50) NOT NULL,
-    firstName text NOT NULL, 
-    lastName text NOT NULL,
-    email text NOT NULL,
+    user_name VARCHAR(50) NOT NULL,
+    first_name text NOT NULL, 
+    last_name text NOT NULL,
+	email varchar(400) ,
     password VARCHAR(255) NOT NULL, -- Assuming hashed passwords stored
     token VARCHAR(255) NOT NULL,
     UNIQUE(email),
@@ -14,3 +15,4 @@ CREATE TABLE user (
 insert into user values("1","ali","ali","eldeep","alieldeep111@gmail.com","123456","aaaaaa");
 
 select * from user;
+

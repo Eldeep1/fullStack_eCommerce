4a86b9c8d528456b32d8110e5e09c850fa7fa3a7
Mohamed Ahmed <muhammed13ca@gmail.com>
2024-04-16 17:47:10 +0200

    Update User.java
    Modified the user class to adhere to Spring Security implementation.
    P.S might want to delete redundant methods later.

--- E-Commerce_backEnd/e_commerce/src/main/java/com/e_commerce/e_commerce/models/User.java
+++ E-Commerce_backEnd/e_commerce/src/main/java/com/e_commerce/e_commerce/models/User.java
@@ -1,10 +1,18 @@
 package com.e_commerce.e_commerce.models;
 
+import java.util.Collection;
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -13,7 +21,7 @@ import jakarta.persistence.Table;
 
 @Entity
 @Table(name="user")
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
@@ -68,10 +76,6 @@ public Map<Object, Object> userToMap() {
         return this.token;
     }
 
-    public String getPassword(){
-        return this.password;
-    }
-
     public String getEmail() {
         return this.email;
     }
@@ -87,5 +91,48 @@ public Map<Object, Object> userToMap() {
 
     public Integer getId() {
         return this.id;
+    }
+    
+    public enum Role {
+        USER,
+        ADMIN
+    }
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public String getPassword(){
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return userName;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 }

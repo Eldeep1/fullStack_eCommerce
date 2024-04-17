bb71b7184e358baf1a64d3f8445243adea1c278d
omarsabri125 <104151494+omarsabri125@users.noreply.github.com>
2024-04-17 10:51:09 +0200

    Update User.java

--- E-Commerce_backEnd/e_commerce/src/main/java/com/e_commerce/e_commerce/models/User.java
+++ E-Commerce_backEnd/e_commerce/src/main/java/com/e_commerce/e_commerce/models/User.java
@@ -2,9 +2,6 @@ package com.e_commerce.e_commerce.models;
 
 import java.util.LinkedHashMap;
 import java.util.Map;
-
-import com.e_commerce.e_commerce.validation.Validation;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -84,27 +81,5 @@ public Map<Object, Object> userToMap() {
 
     public Integer getId() {
         return this.id;
-    }
-    public Map<String,String> Validate(){
-
-        Validation validation= new Validation();
-        Map<String,String> errors= new LinkedHashMap<>();
-    
-        if(!validation.validateFirstName(firstName)){
-            errors.put("firstname","Firstname must contain at least 3 characters");
-        }
-        if(!validation.validateLastName(lastName)){
-            errors.put("lastname","Lastname must contain at least 3 characters");
-        }
-        if (!validation.validateUserName(userName)) {
-            errors.put("username","Username must contain at least 3 characters and a maximum of 20 characters");    
-        }
-        if(!validation.validateEmail(email)){
-            errors.put("email","The email must be in correct format");
-        }
-        if(!validation.validatePassword(password)){
-            errors.put("password","password must contain at least 4 characters and a maximum of 10 characters");
-        }
-        return errors;
     }
 }

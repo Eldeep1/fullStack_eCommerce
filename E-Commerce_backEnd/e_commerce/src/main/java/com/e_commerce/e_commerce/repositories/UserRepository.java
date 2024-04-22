1ca786a8874ee460fbb4553eac8056efb1be3ae5
unknown <alieldeep111@gmail.com>
2024-04-22 04:56:02 +0200

    solving some bugs in resetting password

--- E-Commerce_backEnd/e_commerce/src/main/java/com/e_commerce/e_commerce/repositories/UserRepository.java
+++ E-Commerce_backEnd/e_commerce/src/main/java/com/e_commerce/e_commerce/repositories/UserRepository.java
@@ -2,6 +2,8 @@ package com.e_commerce.e_commerce.repositories;
 
 import com.e_commerce.e_commerce.models.User;
 
+import jakarta.transaction.Transactional;
+
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
@@ -16,10 +18,12 @@ public interface UserRepository extends CrudRepository<User, Integer> {
     @Query("SELECT u FROM User u WHERE u.email = ?1")
     public User findByEmail(String email);
 
+    @Transactional
     @Modifying
-    @Query("UPDATE User u SET u.password = ?2 WHERE u.email = ?1")
-    public Boolean resetPassword(String email, String newPassword);
+    @Query("UPDATE User u SET u.password = ?1 WHERE u.email = ?2")
+    public int resetPassword(String newPassword, String email);
+
     
     @Query("SELECT u FROM User u WHERE u.authQuestionId = ?1 AND u.authQuestionAnswer = ?2 AND u.email = ?3")
-    public Boolean verifyUser(Integer questionId, String authQuestionAnswer, String email);
+    public User verifyUser(String questionId, String authQuestionAnswer, String email);
 }
--- E-Commerce_backEnd/e_commerce/src/main/java/com/e_commerce/e_commerce/services/auth/forget_password/PasswordServices.java
+++ E-Commerce_backEnd/e_commerce/src/main/java/com/e_commerce/e_commerce/services/auth/forget_password/PasswordServices.java
@@ -10,15 +10,21 @@ public class PasswordServices {
     @Autowired
     private UserRepository userRepository ;
 
-    public Boolean verifyUser(Integer questionId, String email, String answer){
-        return userRepository.verifyUser(questionId, answer, email);
+    public Boolean verifyUser(String questionId, String email, String answer){
+        if(userRepository.verifyUser(questionId, answer, email)==null){
+            return false;
+        }
+        return true ;
     }
     public Boolean resetPassword(String newPassword, String email){
-        return userRepository.resetPassword(email, newPassword);
+        
+        return userRepository.resetPassword(newPassword, email)>0;
     }
     public Boolean checkForEmail(String email){
         try {
-            userRepository.findByEmail(email);
+            if (userRepository.findByEmail(email) == null) {
+                return false;
+            };
             return true;   
         } catch (Exception e) {
             return false;
